2790b7503685de89afebce0d7639326cca07da16
Eric DeSimone <eric.desimone@cortland.edu>
2018-05-02 17:28:11 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(sentence)
+    sentence = (%w(sentence))
+    sentence.find do |anagram|
+      anagram.sort == @word
+    end
+end
